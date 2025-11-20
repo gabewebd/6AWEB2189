9b440a3726a2f867d9d0c53d43c15082cf81b0e6
gabewebd <vlsqz.gabrielle@gmail.com>
2025-11-21 02:12:23 +0800

    fix style

--- Prelim/PrelimLabAct2/src/app/app.ts
+++ Prelim/PrelimLabAct2/src/app/app.ts
@@ -1,11 +1,14 @@
+import { Footer } from './footer/footer';
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Skills } from './skills/skills';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Skills, Footer],
   templateUrl: './app.html',
-  styleUrl: '../styles.css'
+  styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-first-app');
--- Prelim/PrelimLabAct2/src/app/header/header.ts
+++ Prelim/PrelimLabAct2/src/app/header/header.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-heade',
+  selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Heade {
+export class Header {
 
 }
