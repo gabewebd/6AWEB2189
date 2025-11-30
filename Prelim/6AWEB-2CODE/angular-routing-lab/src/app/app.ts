58e463a541ab4a823bc53941a59a805d1c7d5b7b
gabewebd <vlsqz.gabrielle@gmail.com>
2025-11-30 23:48:11 +0800

    Added Directives

--- Prelim/6AWEB-2CODE/angular-routing-lab/src/app/app.ts
+++ Prelim/6AWEB-2CODE/angular-routing-lab/src/app/app.ts
@@ -9,4 +9,5 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router'
 })
 export class App {
   protected readonly title = signal('angular-routing-lab');
+  imageUrl1 = 'url(https://cdn.vectorstock.com/i/1000v/58/15/cute-clouds-on-blue-sky-pattern-vector-14535815.jpg)';
 }
--- Prelim/6AWEB-2CODE/angular-routing-lab/src/app/data-binding/data-binding.ts
+++ Prelim/6AWEB-2CODE/angular-routing-lab/src/app/data-binding/data-binding.ts
@@ -10,24 +10,24 @@ import { CommonModule } from '@angular/common'; // Replaced RouterOutlet with Co
   styleUrl: './data-binding.css',
 })
 export class DataBinding {
-  // Interpolation & Main Title (from previous examples)
+  // Interpolation & Main Title 
   message = 'Data Binding Demonstration';
 
   // Property Binding (from previous examples)
   imageUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2EFZwo7jYt7mHhHtHiNaVWjl9RddpSvfjAA&s';
   altText = 'cute cat';
 
-  // Style and Class Binding (from previous examples)
+  // Style and Class Binding 
   textColor = 'blue';
   isHighlighted = true;
 
-  // Two-Way Binding (from previous examples)
+  // Two-Way Binding 
   yourName = '';
 
-  // Event Binding (from previous examples)
+  // Event Binding
   count = 0;
 
-  // Event Binding Methods (from previous examples)
+  // Event Binding Methods 
   increment() {
     this.count++;
   }
@@ -36,12 +36,21 @@ export class DataBinding {
     this.count--;
   }
 
+  // Interpolation
   studentName = "Ainsh";
   score = 95;
+
+  // Property Binding
   imageUrl1 = "https://www.boredpanda.com/blog/wp-content/uploads/2025/07/68761e960552f_cutest-funny-cat-pictures-2.jpg"
   isDisabled = true;
+
+  // Attribute Binding
   colSpanValue = 3;
+
+  // Class Binding
   isPassing = true;
+
+  // Style Binding
   boxColor = "purple";
   boxSize = "150px";
 }
