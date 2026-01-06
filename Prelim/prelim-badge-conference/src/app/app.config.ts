298b7874a5d37b1740d1e1b19a7b1aedb02d532c
gabewebd <vlsqz.gabrielle@gmail.com>
2026-01-06 09:58:29 +0800

    Updated netlify

--- Prelim/prelim-badge-conference/src/app/app.config.ts
+++ Prelim/prelim-badge-conference/src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(
+      routes,
+      // This fix ensures the page scrolls to top on every navigation
+      withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
+    )
+  ]
 };
--- Prelim/prelim-badge-conference/src/app/app.routes.ts
+++ Prelim/prelim-badge-conference/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { PartnersComponent } from './partners/partners.component';
+import { AboutComponent } from './about/about.component';
 import { JoinUsComponent } from './join-us/join-us.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'partners', component: PartnersComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'join-us', component: JoinUsComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', component: PageNotFoundComponent }
 ];
