abd1f34fcd5d24b79e8df7241904acc86a1a0614
gabewebd <vlsqz.gabrielle@gmail.com>
2026-01-06 10:16:54 +0800

    fix

--- Prelim/prelim-badge-conference/src/app/app.component.spec.ts
+++ Prelim/prelim-badge-conference/src/app/app.component.spec.ts
@@ -1,29 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { App } from './app.component';
 
-describe('AppComponent', () => {
+describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [App],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'prelim-badge-conference' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('prelim-badge-conference');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+  it('should render title', async () => {
+    const fixture = TestBed.createComponent(App);
+    await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, prelim-badge-conference');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ainsh');
   });
 });
