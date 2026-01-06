c2de462e04362777f4ca1cd71e941a69e4e4ecdb
gabewebd <vlsqz.gabrielle@gmail.com>
2026-01-06 17:28:03 +0800

    Add Bootstrap types and implement responsive navbar with toggle functionality

--- Prelim/prelim-badge-conference/src/app/header/header.component.ts
+++ Prelim/prelim-badge-conference/src/app/header/header.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild, ElementRef, AfterViewInit, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Collapse } from 'bootstrap';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,40 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {}
+export class HeaderComponent implements AfterViewInit {
+  @ViewChild('navbarNav') navbarNav!: ElementRef;
+  private collapseInstance!: Collapse;
+  
+  // Signal to handle the X animation state
+  isMenuOpen = signal(false);
+
+  ngAfterViewInit() {
+    // Initialize Bootstrap Collapse without automatic toggling
+    this.collapseInstance = new Collapse(this.navbarNav.nativeElement, {
+      toggle: false
+    });
+  }
+
+  toggleMenu(event: Event) {
+    event.stopPropagation(); // Prevents document click from immediately closing it
+    this.isMenuOpen.update(val => !val);
+    this.isMenuOpen() ? this.collapseInstance.show() : this.collapseInstance.hide();
+  }
+
+  closeMenu() {
+    if (this.isMenuOpen()) {
+      this.isMenuOpen.set(false);
+      this.collapseInstance.hide();
+    }
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const clickedInside = this.navbarNav.nativeElement.parentElement?.contains(event.target);
+    
+    // If the menu is open and the user clicks anywhere outside the nav, close it
+    if (this.isMenuOpen() && !clickedInside) {
+      this.closeMenu();
+    }
+  }
+}
