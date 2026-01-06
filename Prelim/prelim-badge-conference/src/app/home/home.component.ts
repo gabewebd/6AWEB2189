5448701ebf9f6ddfd099b5b875557e43fa7e8aa8
gabewebd <vlsqz.gabrielle@gmail.com>
2026-01-06 13:03:21 +0800

    Update image URLs for Seoul and Boston in locations array

--- Prelim/prelim-badge-conference/src/app/home/home.component.ts
+++ Prelim/prelim-badge-conference/src/app/home/home.component.ts
@@ -25,8 +25,8 @@ export class HomeComponent {
 
   locations = signal([
     { name: 'France', track: 'Architecture and Fine Arts', img: 'https://images.unsplash.com/photo-1502602898657-3e91760cbb34?q=80&w=800', desc: 'The largest country in Western Europe, gateway between northern and southern regions.' },
-    { name: 'Seoul', track: 'Humanities and Arts', img: 'https://images.unsplash.com/photo-1538481199705-c710c4e965fc?q=80&w=800', desc: 'Special City of Seoul, the vibrant capital and soul of South Korea.' },
+    { name: 'Seoul', track: 'Humanities and Arts', img: 'https://images.unsplash.com/photo-1506816561089-5cc37b3aa9b0?q=80&w=1275&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', desc: 'Special City of Seoul, the vibrant capital and soul of South Korea.' },
     { name: 'San Francisco', track: 'Science and Technology', img: 'https://images.unsplash.com/photo-1501594907352-04cda38ebc29?q=80&w=800', desc: 'Cultural and financial center, and one of the most cosmopolitan cities.' },
-    { name: 'Boston', track: 'Engineering and Tech', img: 'https://images.unsplash.com/photo-1501949997128-2fdb9f6428f1?q=80&w=800', desc: 'Famous for its history, education, and being a global hub for engineering.' }
+    { name: 'Boston', track: 'Engineering and Tech', img: 'https://plus.unsplash.com/premium_photo-1694475434235-12413ec38b3e?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', desc: 'Famous for its history, education, and being a global hub for engineering.' }
   ]);
 }
